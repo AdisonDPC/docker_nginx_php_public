f7ba963dab0fbb188fc76674ebd86596927109de
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2020-10-07 17:02:48 +0100

    Update Commit - 07/10/2020 - Testing Travis 05.

--- monitor.sh
+++ monitor.sh
@@ -6,4 +6,6 @@ NAME=nginx/php
 TAG=1.0
 PORT_80=8080
 
+# Monitor docker container.
+
 docker exec -it ${AUTHOR}_$(echo "${NAME}" | tr '/' '_') bash
--- stop.sh
+++ stop.sh
@@ -5,4 +5,6 @@ AUTHOR=adpc
 NAME=nginx/php
 TAG=1.0
 
+# Stop docker container.
+
 docker stop ${AUTHOR}_$(echo "${NAME}" | tr '/' '_')
